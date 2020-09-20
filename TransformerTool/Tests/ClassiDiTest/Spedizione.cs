28bc6a6213509d09fb00815864f21ca0f79162e5
zuk95 <azuccolo02@gmail.com>
2020-09-21 00:45:10 +0200

    aggiunta Spedizione e nuovo test con un db di prova

---
+++ TransformerTool/Tests/ClassiDiTest/Spedizione.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.ClassiDiTest
+{
+    public class Spedizione
+    {
+        public string CodiceSpedizione { get; set; }
+        public DateTime DataAsp { get; set; }
+        public string CittàDestinatario { get; set; }
+        public string CittàPartenza { get; set; }
+        public decimal QuantitàTotale { get; set; }
+    }
+}
---
+++ TransformerTool/Tests/TestConDb.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using Tests.ClassiDiTest;
+using TransformerTool;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestConDb
+    {
+        [TestMethod]
+        public void ConnessioneDatabase()
+        {
+            MySqlConnection connection = new MySqlConnection("Server = localhost; Database = prova; Uid = administrator; Pwd = Arcadinoe95; ");
+
+            connection.Open();
+            Assert.IsTrue(connection.State == ConnectionState.Open);
+
+            connection.Close();
+            Assert.IsTrue(connection.State == ConnectionState.Closed);
+        }
+
+        [TestMethod]
+        public void TransformDataTableFromQuery()
+        {
+            MySqlConnection connection = new MySqlConnection("Server = localhost; Database = prova; Uid = administrator; Pwd = Arcadinoe95; ");
+            connection.Open();
+
+            string query = "select spcod as CodiceSpedizione, datasp as DataAsp, cittadest as CittàDestinatario, cittapart as CittàPartenza, qta_totale as QuantitàTotale  from spedizioni;";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            connection.Close();
+
+            Transformer<Spedizione> transformer = new Transformer<Spedizione>();
+            List<Spedizione> risultato = transformer.Transform(dataTable);
+
+            Assert.IsNotNull(risultato);
+            Assert.IsTrue(risultato.Count > 0);
+
+            Assert.IsTrue(dataTable.Rows.Count > 0);
+
+
+
+
+        }
+    }
+}
